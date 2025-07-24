68eb99b0074d3199a1fc7952ff5a598514b68bfa
Spylher <pedro.pepe.5050@gmail.com>
2025-07-24 19:21:12 -0300

    Added grf files.

--- MySpyGRF.Core/RegisterRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MySpyGRF.Core;
-
-public class RegisterRequest
-{
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-}
--- MySpyGRF.Server/Services/IUserService.cs
+++
@@ -1,7 +0,0 @@
-﻿using MySpyGRF.Server.Data.Models;
-namespace MySpyGRF.Server.Services;
-public interface IUserService
-{
-    Task<UserEntity> RegisterAsync(string username, string password, CancellationToken ct);
-    Task<UserEntity?> AuthenticateAsync(string username, string password, CancellationToken ct);
-}
